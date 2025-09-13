f7fa4718831dc6582dd9cb6ca73174816418f806
AwaisMiskeen <160594692+AwaisMiskeen@users.noreply.github.com>
2025-09-13 16:34:02 +0500

    update in if statement

--- PROJECT_01/main.py
+++ PROJECT_01/main.py
@@ -1,31 +1,37 @@
-# Rock,Paper, Sizer game.
+# Rock,Paper, Scissor game.
 '''
 -1 for Rock
 0 for Paper
-1 for Sizer
+1 for Scissor
 
 '''
 
 import random # import random for computer chose randome number as given
 
-computer = random.choice([-1 , 0 , 1]) 
-print("Chose any one of these: (r , p ,s)")
-youstr = input("Enter your choice: ")
+computer = random.choice([-1 , 0 , 1])
 youdict = {"r": -1 , "p" : 0 , "s" : 1}
-reversdict = { -1:"Rock" , 0:"Paper" , 1:"Sizer"}
-you = youdict[youstr]
+reversdict = { -1:"Rock" , 0:"Paper" , 1: "Scissor"}
 
+#  this while loop for valid input the user
+while True:
+    youstr = input("Choose one (r = Rock, p = Paper, s = Scissor): ").lower()
+    if youstr in youdict:
+        break  
+    else:
+        print("You chose a wrong key! Please choose from 'r', 'p', or 's'.")
+
+you = youdict[youstr]
 print(f"You chose {reversdict[you]}\n Computer chose {reversdict[computer]}")
 
 if(computer == you):
     print("Game Draw")
 else:
-    if(computer == -1 and you == 0): 
-        print("You Lose!")
+    if(computer == -1 and you == 0): #
+        print("You Win!")
     elif(computer == -1 and you == 1):
         print("You Lose!")
     elif(computer == 0 and you == -1):
-        print("You Win!")
+        print("You Lose!")
     elif(computer == 0 and you == 1):
         print("You Win!")
     elif(computer == 1 and you == -1):
@@ -34,3 +40,8 @@ else:
         print("You Lose!")
     else:
         print("Something went wrong!")
+
+
+#  -1 for Rock
+# 0 for Paper
+# 1 for Scissor
